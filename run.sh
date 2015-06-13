f0daaba7e4f3a9b77d6c90faf7ac70c2678271bf
minhoolee <minhoo2000@gmail.com>
2015-06-13 14:58:18 -0700

    Fix script so that executable is only run when it compiles correctly

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # Compile library
-g++ main.cpp build/lazer-vision.so -o vision `pkg-config opencv --cflags --libs` --std=c++11 -I/usr/local/include/libfreenect -lfreenect -lfreenect_sync
+if g++ main.cpp build/lazer-vision.so -o vision `pkg-config opencv --cflags --libs` --std=c++11 -I/usr/local/include/libfreenect -lfreenect -lfreenect_sync; then
 
 # Run executable using fakenect
 fakenect thanksgiving0 ./vision
-
+fi
 
 # Run executable using fakenect, but loop it
 # while :
 # do
-# 	echo $(fakenect fakenect-testing/thanksgiving0 ./vision) > run.log
+# 	echo $(fakenect thanksgiving0 ./vision) > run.log
 # 
 # 	# Search log for appropriate ending to fakenect
 # 	if grep -Fxq "$FILENAME" run.log
