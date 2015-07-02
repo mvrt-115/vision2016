7f970f8e654d1d23da550cf309e87dba76594a6d
minhoolee <minhoo2000@gmail.com>
2015-07-01 22:22:34 -0700

    Clean up input code and move includes to header file

--- include/input/freenect_cv.hpp
+++ include/input/freenect_cv.hpp
@@ -3,6 +3,14 @@
 
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <stdexcept>
+#ifdef __linux__
+#include <libfreenect_sync.h>
+#endif
+#ifdef __APPLE__
+#include <libfreenect/libfreenect_sync.h>
+#endif
+
 cv::Mat kinectDepth(int index);
 cv::Mat kinectRGB(int index);
 cv::Mat kinectIR(int index);
