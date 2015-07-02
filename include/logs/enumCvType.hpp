0f09b59cc330414a5f00d84d4b9beab5880f58ec
minhoolee <minhoo2000@gmail.com>
2015-07-01 22:18:43 -0700

    Add code that can list the cv type of the matrix image for deeper debugging

---
+++ include/logs/enumCvType.hpp
@@ -0,0 +1,14 @@
+#ifndef ENUM_CV_TYPE_HPP
+#define ENUM_CV_TYPE_HPP
+	
+#include <iostream>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cmath>
+#include <ctime>
+
+std::string enumCvType(cv::Mat &img);
+
+#endif // ENUM_CV_TYPE_HPP
